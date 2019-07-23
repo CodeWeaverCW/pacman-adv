d8b3a438e90f383bfd20b92b0fda8ec3b8d6ddfe
Logan "CodeWeaver" Hall <codeweaver@loganhall.net>
2019-07-23 15:05:15 -0500

    Initial commit

---
+++ src/Entity.h
@@ -0,0 +1,21 @@
+/*
+	Entity.h
+Author: Logan "CodeWeaver" Hall
+2019-07-23
+
+This is the top-level class for objects that may exist in the maze. This includes
+Pac-Man, the ghosts, fruit, and powerups. This does *not* include dots, which are
+maze data.
+*/
+
+
+class Entity {
+public:
+	/* Constructors / Destructor */
+	Entity();
+	~Entity() = {};
+
+	/* Data */
+	position
+	velocity
+}
---
+++ src/SysDep.h
@@ -0,0 +1,26 @@
+/*
+	SysDep.h
+Author: Logan "CodeWeaver" Hall
+2019-07-23
+
+This is the namespace for "system dependent" code. Ideally, the program can
+rely on these functions to interface with the target platform. At best,
+to "port" the program to any device, you only need to implement these functions
+with platform-specific code; on the flipside, it should be assured that a
+"clean" port is impossible on systems that cannot fully implement them.
+Building for a target device is as simple as including the respective `.cpp`
+which implements these functions for the device.
+
+SysDep provisions for audio, graphics, input, and various system utilities.
+
+This is not meant to discourage developers from using macros to supply
+system-specific code. This namespace is meant to centralize the code that "must"
+be ported, while platform-specific macros are discretionary (for e.g. platform-
+exclusive features).
+*/
+
+
+namespace SysDep {
+	/* Utilities */
+	// `int main()` is considered "system-dependent", so don't forget to supply that too!
+}
