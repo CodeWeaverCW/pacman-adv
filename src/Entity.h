aa2c5999eda07d7a081740ef067e2bb8f56b5922
Logan "CodeWeaver" Hall <codeweaver@loganhall.net>
2019-07-25 04:39:00 -0500

    Finished defining the Entity class; will implement tomorrow

--- src/Entity.h
+++ src/Entity.h
@@ -8,6 +8,8 @@ Pac-Man, the ghosts, fruit, and powerups. This does *not* include dots, which ar
 maze data.
 */
 
+#include "Maze.h"
+
 
 class Entity {
 public:
@@ -15,7 +17,62 @@ public:
 	Entity();
 	~Entity() = {};
 
+
+	/* Methods */
+	static uint8_t distance(Entity& one, Entity& two);
+
+
+	/* Properties */
+	// Get tile coordinate
+	inline uint8_t xTile(void);
+	inline uint8_t yTile(void);
+	inline uint8_t zTile(void);
+	// Get pixel coordinate
+	inline uint16_t xPixel(void);
+	inline uint16_t yPixel(void);
+	inline uint16_t zPixel(void);
+	// Center this entity on the specified tile
+	inline void setOnTile(uint8_t x, uint8_t y, uint8_t z);
+	// Place this entity on an exact pixel location
+	inline void setOnPixel(uint16_t x, uint16_t y, uint16_t z);
+
+	// Speed
+	inline void setSpeedFactor(float percent);
+
+protected:
+	// Direction
+	using Maze::Direction;
+	inline bool facing(Direction dir);
+
+
+private:
 	/* Data */
-	position
-	velocity
+	/* Position
+	Coordinates are precise to a sixteenth of a pixel. In other words,
+	the coordinate system is in a 12.4 fixed-point notation.
+
+	Use protected methods to extract appropriate data.
+	*/
+	uint16_t x;
+	uint16_t y;
+	uint16_t z;
+
+	/* Speed
+	Speed is measured in sixteenth-pixels per tick.
+	Movement needs to be calculated for several ticks in advance
+	on systems that run less than 60 FPS.
+	*/
+	uint8_t speed;
+	static const uint8_t base_speed = 20;	// 75 pixels/second, at 60 ticks/second
+
+	/* Direction
+	The direction facing is represented by four bitflags, in order of preference:
+		up, left, down, right
+	For example, a direction of 0b0100 means the entity is moving left.
+	A direction of e.g. 0b1001 (up, right) is possible, such as when Pac-Man is cornering.
+
+	Direction constants (bitmasks) are defined in the Maze class, as a tile's possible directions
+	are listed in the same format.
+	*/
+	uint8_t direction;
 }
