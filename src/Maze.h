fbeedf2065cf4ba586a4b3a30a44697b6dc31509
Logan "CodeWeaver" Hall <codeweaver@loganhall.net>
2019-07-29 23:05:18 -0500

    Init

---
+++ src/Maze.h
@@ -0,0 +1,27 @@
+/*
+	Maze.h
+Author: Logan "CodeWeaver" Hall
+2019-07-29
+
+
+*/
+
+#include "Coord.h"
+#include "Entity.h"
+
+
+class Maze {
+public:
+	/* Constructors / Destructor */
+	Maze(const char* filename);
+	~Maze() = {}
+
+	/* Methods */
+	void tick();	// Process game state for this tick
+
+
+private:
+	/* Data */
+	Tile tiles[];
+	Entity* entities[];
+}
